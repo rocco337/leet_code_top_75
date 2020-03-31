6d50ff144b1eb5d87bbf34856c72873fca180a5d
roko bobic <roko.bobic@gmail.com>
2020-03-31 16:49:30 +0200

    implement best time to buy and sell stock

---
+++ 02_buy_sell_stock/02_buy_sell_stock_test.go
@@ -0,0 +1,40 @@
+package buysellstock
+
+import (
+	"testing"
+)
+
+func TestHuffman(t *testing.T) {
+
+	t.Run("Find max profit - should be 5", func(t *testing.T) {
+		prices := []int{7, 1, 5, 3, 6, 4}
+		expected := 5
+
+		actual := MaxProfit(prices)
+		if actual != expected {
+			t.Error("Expected: ", expected, "Actual: ", actual)
+		}
+	})
+
+	t.Run("Find max profit - should be 0", func(t *testing.T) {
+		prices := []int{77, 6, 4, 3, 1}
+		expected := 0
+
+		actual := MaxProfit(prices)
+		if actual != expected {
+			t.Error("Expected: ", expected, "Actual: ", actual)
+		}
+	})
+}
+
+func BenchmarkHuffman(b *testing.B) {
+	prices := []int{7, 1, 5, 3, 6, 4}
+	expected := 5
+
+	for n := 0; n < b.N; n++ {
+		actual := MaxProfit(prices)
+		if actual != expected {
+			b.Error("Expected: ", expected, "Actual: ", actual)
+		}
+	}
+}
